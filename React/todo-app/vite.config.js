c40f7b7fe4742de3e6c932cceb5ac6f29888d270
kevin-rodriguez-dev <kevinrodrihid2003@gmail.com>
2024-10-21 16:21:55 -0600

    Initial commit to deploy all-app

--- React/todo-app/vite.config.js
+++ React/todo-app/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bootcamp-fsj-25/React/todo-app/', // Ruta base para GitHub Pages
 })
